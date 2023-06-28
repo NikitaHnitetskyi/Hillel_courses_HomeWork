3de0e6567d68243452e8c75748d16fb1e4e8dfaf
Nikita_Hnitetskyi <106909771+NikitaHnitetskyi@users.noreply.github.com>
2023-06-28 21:22:49 +0300

    code usage example delegate and event

---
+++ Delegate_Event/ArrayHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Event
+{
+    internal class ArrayHandler
+    {
+        private int[] numbers;
+
+        public ArrayHandler(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public event EventHandler<int> SquareRootIsInteger;
+
+        public IEnumerable<double> SquareRoots(Func<int, bool> filter)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (filter(numbers[i]))
+                {
+                    double root = numbers[i].Sqrt();
+                    if (root % 1 == 0)
+                        SquareRootIsInteger?.Invoke(this, numbers[i]);
+                    yield return root;
+                }
+            }
+        }
+    }
+}
+
---
+++ Delegate_Event/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Delegate_Event;
+
+namespace ClassesForDataBase
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 10, 25, 30, 50 };
+            ArrayHandler handler = new ArrayHandler(numbers);
+            handler.SquareRootIsInteger += OnSquareRootIsInteger;
+
+
+
+
+            Func<int, bool> filter = num => num > 5;
+
+            var roots = handler.SquareRoots(filter).ToArray();
+
+            Console.WriteLine("Результат:");
+            
+            for (int i = 0; i < roots.Length; i++)
+            {
+                Console.WriteLine($"Корень из {numbers[i]} = {roots[i]}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void OnSquareRootIsInteger(object sender, int integerRoot)
+        {
+            Console.WriteLine($"Корень числа {integerRoot} - целое число");
+        }
+    }
+}
